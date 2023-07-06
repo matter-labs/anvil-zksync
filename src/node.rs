15da86b92995fd7b9e12eebdd79aa725dc501cd6
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-07-06 10:53:18 -0500

    chore: run fmt

--- src/node.rs
+++ src/node.rs
@@ -21,7 +21,7 @@ use zksync_contracts::{
 };
 use zksync_core::api_server::web3::backend_jsonrpc::namespaces::eth::EthNamespaceT;
 use zksync_types::{
-    api::{TransactionReceipt, TransactionVariant, Log},
+    api::{Log, TransactionReceipt, TransactionVariant},
     get_code_key, get_nonce_key,
     l2::L2Tx,
     transaction_request::{l2_tx_from_call_req, TransactionRequest},
@@ -30,7 +30,7 @@ use zksync_types::{
     vm_trace::VmTrace,
     zk_evm::block_properties::BlockProperties,
     StorageKey, StorageLogQueryType, Transaction, ACCOUNT_CODE_STORAGE_ADDRESS,
-    L2_ETH_TOKEN_ADDRESS
+    L2_ETH_TOKEN_ADDRESS,
 };
 use zksync_utils::{
     bytecode::hash_bytecode, bytes_to_be_words, h256_to_account_address, h256_to_u256, h256_to_u64,
@@ -314,7 +314,7 @@ impl InMemoryNode {
         HashMap<StorageKey, H256>,
         VmTxExecutionResult,
         BlockInfo,
-        HashMap<U256, Vec<U256>>
+        HashMap<U256, Vec<U256>>,
     ) {
         let inner = self.inner.write().unwrap();
 
@@ -345,7 +345,7 @@ impl InMemoryNode {
             bootloader_code,
             execution_mode,
         );
-        
+
         let tx: Transaction = l2_tx.into();
 
         push_transaction_to_bootloader_memory(&mut vm, &tx, execution_mode, None);
@@ -588,35 +588,42 @@ impl EthNamespaceT for InMemoryNode {
                 U64::from(0)
             };
 
-            let logs: Vec<Log> = info.result.result.logs.events.iter().map(|log| {
-                let address = log.address;
-                let topics = &log.indexed_topics;
-                let data = log.value.clone();
-                let block_hash = Some(hash);
-                let block_number = Some(U64::from(info.miniblock_number));
-                let l1_batch_number = Some(U64::from(info.batch_number as u64));
-                let transaction_hash = Some(hash);
-                let transaction_index = Some(U64::from(1).into());
-                let log_index = Some(U256::default());
-                let transaction_log_index = Some(U256::default());
-                let log_type = None;
-                let removed = None;
-            
-                Log {
-                    address,
-                    topics: topics.to_vec(),
-                    data: zksync_types::Bytes(data),
-                    block_hash,
-                    block_number,
-                    l1_batch_number,
-                    transaction_hash,
-                    transaction_index,
-                    log_index,
-                    transaction_log_index,
-                    log_type,
-                    removed,
-                }
-            }).collect();
+            let logs: Vec<Log> = info
+                .result
+                .result
+                .logs
+                .events
+                .iter()
+                .map(|log| {
+                    let address = log.address;
+                    let topics = &log.indexed_topics;
+                    let data = log.value.clone();
+                    let block_hash = Some(hash);
+                    let block_number = Some(U64::from(info.miniblock_number));
+                    let l1_batch_number = Some(U64::from(info.batch_number as u64));
+                    let transaction_hash = Some(hash);
+                    let transaction_index = Some(U64::from(1).into());
+                    let log_index = Some(U256::default());
+                    let transaction_log_index = Some(U256::default());
+                    let log_type = None;
+                    let removed = None;
+
+                    Log {
+                        address,
+                        topics: topics.to_vec(),
+                        data: zksync_types::Bytes(data),
+                        block_hash,
+                        block_number,
+                        l1_batch_number,
+                        transaction_hash,
+                        transaction_index,
+                        log_index,
+                        transaction_log_index,
+                        log_type,
+                        removed,
+                    }
+                })
+                .collect();
 
             TransactionReceipt {
                 transaction_hash: hash,
