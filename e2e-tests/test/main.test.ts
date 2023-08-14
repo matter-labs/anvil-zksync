1b1fb222787d407df228855eee0576610f23604a
MexicanAce <nicolasvillanueva@msn.com>
2023-08-14 17:04:42 +0100

    DO NOT MERGE. Failing test for verification evidence.

--- e2e-tests/test/main.test.ts
+++ e2e-tests/test/main.test.ts
@@ -13,85 +13,12 @@ async function deployGreeter(deployer: Deployer): Promise<Contract> {
 
 describe('Greeter', function () {
   it("Should return the new greeting once it's changed", async function () {
-    // const provider = Provider.getDefaultProvider();
-    console.log("Latest version");
-    console.log("Connecting directly to: http://localhost:8011");
     const provider = new Provider('http://127.0.0.1:8011');
-    console.log("Provider created");
-    
+
     const wallet = new Wallet(RICH_WALLET_PK, provider);
-    console.log("Wallet connected");
     const deployer = new Deployer(hre, wallet);
-    console.log("Deployer configured");
-    console.log(provider.connection);
-
-    // const test = await provider.getNetwork();
-    // console.log(test);
-
-    console.log("Starting manual curl request");
-    const http = require('http');
-
-    const data = JSON.stringify({
-      jsonrpc: "2.0",
-      id: "2",
-      method: "eth_call",
-      params: [{
-        to: "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049",
-        data: "0x0000",
-        from: "0xa61464658AfeAf65CccaaFD3a512b69A83B77618",
-        gas: "0x0000",
-        gasPrice: "0x0000",
-        value: "0x0000",
-        nonce: "0x0000"
-      }, "latest"]
-    });
-
-    const options = {
-      hostname: '127.0.0.1',
-      port: 8011,
-      path: '/',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-      }
-    };
-
-    const req = http.request(options, (res) => {
-      let chunks = [];
-
-      res.on('data', (chunk) => {
-        // @ts-ignore
-        chunks.push(chunk);
-      });
-
-      res.on('end', () => {
-        const responseBody = Buffer.concat(chunks).toString('utf-8');
-        console.log(responseBody);
-      });
-    });
-
-    req.on('error', (error) => {
-      console.error('Error:', error.message);
-    });
-
-    req.write(data);
-    req.end();
-
-
-
-
-
-
-
-
-
-
-
-    
 
     const greeter = await deployGreeter(deployer);
-    console.log("Greeting deployed");
 
     expect(await greeter.greet()).to.eq('Hi');
 
@@ -99,6 +26,6 @@ describe('Greeter', function () {
     // wait until the transaction is mined
     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal('Hola, mundo!');
+    expect(await greeter.greet()).to.equal('Hola, mundo2!');
   });
 });
--- scripts/execute-e2e-tests.sh
+++ scripts/execute-e2e-tests.sh
@@ -21,9 +21,6 @@ while [ $COUNTER -lt $MAX_RETRIES ]; do
     # Check if the request was successful
     if [ "$RESPONSE" -eq 200 ]; then
         echo "Node is running! Starting tests..."
-        # TODO: Remove this, just debugging for now
-        RESPONSE=$(curl -s -X POST -H "content-type: application/json" -d "$DATA" $URL || true)
-        echo $RESPONSE
         break
     else
         echo "Node not ready, retrying in 1 second..."
@@ -58,8 +55,4 @@ echo ""
 echo "================="
 echo "Running e2e tests"
 echo "================="
-# TODO: Remove this, just debugging for now
-RESPONSE=$(curl --request POST --url http://localhost:8011/ --header 'content-type: application/json' --data '{  "jsonrpc": "2.0",    "id": "2",    "method": "eth_call",    "params": [{        "to": "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049",        "data": "0x0000",        "from": "0xa61464658AfeAf65CccaaFD3a512b69A83B77618",        "gas": "0x0000",        "gasPrice": "0x0000",        "value": "0x0000",        "nonce": "0x0000"    }, "latest"]}')
-echo $RESPONSE
-
 yarn test
