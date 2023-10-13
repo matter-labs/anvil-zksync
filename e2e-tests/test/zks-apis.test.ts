ddc0e39138d8914ed4f5d890dc45a08be2d1c288
George W <140627974+grw-ms@users.noreply.github.com>
2023-10-13 12:48:56 +0200

    Update e2e-tests/test/zks-apis.test.ts
    Co-authored-by: Nicolas Villanueva <nicolasvillanueva@msn.com>

--- e2e-tests/test/zks-apis.test.ts
+++ e2e-tests/test/zks-apis.test.ts
@@ -61,7 +61,7 @@ describe("zks_getTokenPrice", function () {
 });
 
 describe("zks_getTransactionDetails", function () {
-  it.only("Should return transaction details for locally-executed transactions", async function () {
+  it("Should return transaction details for locally-executed transactions", async function () {
     const wallet = new Wallet(RichAccounts[0].PrivateKey);
     const deployer = new Deployer(hre, wallet);
 
