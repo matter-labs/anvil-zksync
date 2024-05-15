7d551e74665dab6c01b674ac4dd48e2c0442c3b5
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-05-15 11:21:37 +0200

    fix unit tests

--- src/node/zks.rs
+++ src/node/zks.rs
@@ -590,7 +590,7 @@ mod tests {
 
         let result = node.estimate_fee(mock_request).await.unwrap();
 
-        assert_eq!(result.gas_limit, U256::from(2618937));
+        assert_eq!(result.gas_limit, U256::from(6793236));
         assert_eq!(result.max_fee_per_gas, U256::from(50000000));
         assert_eq!(result.max_priority_fee_per_gas, U256::from(0));
         assert_eq!(result.gas_per_pubdata_limit, U256::from(37500));
