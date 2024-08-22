41dac73e557fb7262784604db65ec2f32268e717
mm <mm@matterlabs.dev>
2024-08-22 11:24:42 +0200

    cleaned up error handling

--- src/bytecode_override.rs
+++ src/bytecode_override.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 use crate::fork::ForkSource;
 use crate::node::InMemoryNode;
+use eyre::Context;
 use hex::FromHex;
 use serde::Deserialize;
 use std::str::FromStr;
@@ -17,10 +18,12 @@ struct Bytecode {
     object: String,
 }
 
+// Loads a list of bytecodes and addresses from the directory and then inserts them directly
+// into the Node's storage.
 pub fn override_bytecodes<T: Clone + ForkSource + std::fmt::Debug>(
     node: &InMemoryNode<T>,
     bytecodes_dir: String,
-) -> Result<(), anyhow::Error> {
+) -> eyre::Result<()> {
     for entry in fs::read_dir(bytecodes_dir)? {
         let entry = entry?;
         let path = entry.path();
@@ -28,20 +31,25 @@ pub fn override_bytecodes<T: Clone + ForkSource + std::fmt::Debug>(
         if path.is_file() {
             let filename = match path.file_name().and_then(|name| name.to_str()) {
                 Some(name) => name,
-                None => anyhow::bail!("Invalid filename {}", path.display().to_string()),
+                None => eyre::bail!("Invalid filename {}", path.display().to_string()),
             };
 
-            let filename = filename.strip_suffix(".json").unwrap();
+            // Look only at .json files.
+            if let Some(filename) = filename.strip_suffix(".json") {
+                let address = Address::from_str(filename)
+                    .wrap_err(format!("Cannot parse {} as address", filename))?;
 
-            let address = Address::from_str(filename).expect(&format!("Cannot parse {}", filename));
+                let file_content = fs::read_to_string(&path)?;
+                let contract: ContractJson = serde_json::from_str(&file_content)
+                    .wrap_err(format!("Failed to  parse json file {:?}", path))?;
 
-            let file_content = fs::read_to_string(&path)?;
-            let contract: ContractJson = serde_json::from_str(&file_content).unwrap();
+                let bytecode = Vec::from_hex(contract.bytecode.object)
+                    .wrap_err(format!("Failed to parse hex from {:?}", path))?;
 
-            let bytecode = Vec::from_hex(contract.bytecode.object).unwrap();
-
-            node.override_bytecode(&address, &bytecode).unwrap();
-            tracing::info!("+++++ Replacing bytecode at address {:?} +++++", address);
+                node.override_bytecode(&address, &bytecode)
+                    .expect("Failed to override bytecode");
+                tracing::info!("+++++ Replacing bytecode at address {:?} +++++", address);
+            }
         }
     }
     Ok(())
