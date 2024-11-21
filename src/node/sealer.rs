4f604da8db277f391b5a84773322741535b91c58
Daniyar Itegulov <ditegulov@gmail.com>
2024-11-21 18:13:06 +1100

    add unit tests for `BlockSealer`

--- src/node/sealer.rs
+++ src/node/sealer.rs
@@ -78,3 +78,220 @@ impl FixedTimeBlockSealer {
         Poll::Pending
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::node::pool::TxBatch;
+    use crate::node::{BlockSealer, ImpersonationManager, TxPool};
+    use std::ptr;
+    use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+    use std::time::Duration;
+
+    const NOOP: RawWaker = {
+        const VTABLE: RawWakerVTable = RawWakerVTable::new(
+            // Cloning just returns a new no-op raw waker
+            |_| NOOP,
+            // `wake` does nothing
+            |_| {},
+            // `wake_by_ref` does nothing
+            |_| {},
+            // Dropping does nothing as we don't allocate anything
+            |_| {},
+        );
+        RawWaker::new(ptr::null(), &VTABLE)
+    };
+    const WAKER_NOOP: Waker = unsafe { Waker::from_raw(NOOP) };
+
+    #[test]
+    fn immediate_empty() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::immediate(1000);
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+    }
+
+    #[test]
+    fn immediate_one_tx() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::immediate(1000);
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        let [tx] = pool.populate::<1>();
+
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs: vec![tx]
+            })
+        );
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+    }
+
+    #[test]
+    fn immediate_several_txs() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::immediate(1000);
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        let txs = pool.populate::<10>();
+
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs: txs.to_vec()
+            })
+        );
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+    }
+
+    #[test]
+    fn immediate_respect_max_txs() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::immediate(3);
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        let txs = pool.populate::<10>();
+
+        for txs in txs.chunks(3) {
+            assert_eq!(
+                block_sealer.poll(&pool, &mut cx),
+                Poll::Ready(TxBatch {
+                    impersonating: false,
+                    txs: txs.to_vec()
+                })
+            );
+        }
+    }
+
+    #[test]
+    fn immediate_gradual_txs() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::immediate(1000);
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        // Txs are added to the pool in small chunks
+        let txs0 = pool.populate::<3>();
+        let txs1 = pool.populate::<4>();
+        let txs2 = pool.populate::<5>();
+
+        let mut txs = txs0.to_vec();
+        txs.extend(txs1);
+        txs.extend(txs2);
+
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs,
+            })
+        );
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+
+        // Txs added after the first poll should be available for sealing
+        let txs = pool.populate::<10>().to_vec();
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs,
+            })
+        );
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+    }
+
+    #[tokio::test]
+    async fn fixed_time_very_long() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::fixed_time(1000, Duration::from_secs(10000));
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+    }
+
+    #[tokio::test]
+    async fn fixed_time_seal_empty() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::fixed_time(1000, Duration::from_millis(100));
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        // Sleep enough time to (theoretically) produce at least 2 blocks
+        tokio::time::sleep(Duration::from_millis(250)).await;
+
+        // Sealer should seal one empty block when polled and then refuse to seal another one
+        // shortly after as it ensures enough time passes in-between of blocks.
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs: vec![]
+            })
+        );
+        assert_eq!(block_sealer.poll(&pool, &mut cx), Poll::Pending);
+
+        // Sleep enough time to produce one block
+        tokio::time::sleep(Duration::from_millis(150)).await;
+
+        // Next block should be sealable
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs: vec![]
+            })
+        );
+    }
+
+    #[tokio::test]
+    async fn fixed_time_seal_with_txs() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::fixed_time(1000, Duration::from_millis(100));
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        let txs = pool.populate::<3>();
+
+        // Sleep enough time to produce one block
+        tokio::time::sleep(Duration::from_millis(150)).await;
+
+        assert_eq!(
+            block_sealer.poll(&pool, &mut cx),
+            Poll::Ready(TxBatch {
+                impersonating: false,
+                txs: txs.to_vec()
+            })
+        );
+    }
+
+    #[tokio::test]
+    async fn fixed_time_respect_max_txs() {
+        let pool = TxPool::new(ImpersonationManager::default());
+        let mut block_sealer = BlockSealer::fixed_time(3, Duration::from_millis(100));
+        let waker = &WAKER_NOOP;
+        let mut cx = Context::from_waker(&waker);
+
+        let txs = pool.populate::<10>();
+
+        for txs in txs.chunks(3) {
+            // Sleep enough time to produce one block
+            tokio::time::sleep(Duration::from_millis(150)).await;
+
+            assert_eq!(
+                block_sealer.poll(&pool, &mut cx),
+                Poll::Ready(TxBatch {
+                    impersonating: false,
+                    txs: txs.to_vec()
+                })
+            );
+        }
+    }
+}
