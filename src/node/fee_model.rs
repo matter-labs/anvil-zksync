d364974782f615b5a6eb5afb1584039cf5ac2f8c
Daniyar Itegulov <ditegulov@gmail.com>
2024-11-29 15:10:57 +1100

    slightly refactor `TestNodeFeeInputProvider`

--- src/node/fee_model.rs
+++ src/node/fee_model.rs
@@ -1,23 +1,41 @@
-use zksync_types::fee_model::{BatchFeeInput, FeeModelConfigV2, FeeParams, FeeParamsV2};
+use zksync_multivm::utils::derive_base_fee_and_gas_per_pubdata;
+use zksync_multivm::VmVersion;
+use zksync_types::fee_model::{
+    BaseTokenConversionRatio, BatchFeeInput, FeeModelConfigV2, FeeParams, FeeParamsV2,
+};
 
 use crate::config::constants::{
     DEFAULT_ESTIMATE_GAS_PRICE_SCALE_FACTOR, DEFAULT_ESTIMATE_GAS_SCALE_FACTOR,
     DEFAULT_FAIR_PUBDATA_PRICE, DEFAULT_L1_GAS_PRICE, DEFAULT_L2_GAS_PRICE,
 };
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct TestNodeFeeInputProvider {
-    pub l1_gas_price: u64,
-    pub l1_pubdata_price: u64,
-    pub l2_gas_price: u64,
-    pub compute_overhead_part: f64,
-    pub pubdata_overhead_part: f64,
-    pub batch_overhead_l1_gas: u64,
-    pub max_gas_per_batch: u64,
-    pub max_pubdata_per_batch: u64,
     /// L1 Gas Price Scale Factor for gas estimation.
     pub estimate_gas_price_scale_factor: f64,
     /// The factor by which to scale the gasLimit.
     pub estimate_gas_scale_factor: f32,
+
+    fee_params: FeeParamsV2,
+}
+
+// TODO: Derive PartialEq for `FeeParamsV2` in upstream
+impl PartialEq for TestNodeFeeInputProvider {
+    fn eq(&self, other: &Self) -> bool {
+        fn eq_config(a: FeeModelConfigV2, b: FeeModelConfigV2) -> bool {
+            a.minimal_l2_gas_price == b.minimal_l2_gas_price
+                && a.compute_overhead_part == b.compute_overhead_part
+                && a.pubdata_overhead_part == b.pubdata_overhead_part
+                && a.batch_overhead_l1_gas == b.batch_overhead_l1_gas
+                && a.max_gas_per_batch == b.max_gas_per_batch
+                && a.max_pubdata_per_batch == b.max_pubdata_per_batch
+        }
+
+        self.estimate_gas_price_scale_factor == other.estimate_gas_price_scale_factor
+            && self.estimate_gas_scale_factor == other.estimate_gas_scale_factor
+            && self.fee_params.l1_gas_price() == other.fee_params.l1_gas_price()
+            && self.fee_params.l1_pubdata_price() == other.fee_params.l1_pubdata_price()
+            && eq_config(self.fee_params.config(), other.fee_params.config())
+    }
 }
 
 impl TestNodeFeeInputProvider {
@@ -29,16 +47,9 @@ impl TestNodeFeeInputProvider {
         match fee_params {
             FeeParams::V1(_) => todo!(),
             FeeParams::V2(fee_params) => Self {
-                l1_gas_price: fee_params.l1_gas_price(),
-                l1_pubdata_price: fee_params.l1_pubdata_price(),
-                l2_gas_price: fee_params.config().minimal_l2_gas_price,
-                compute_overhead_part: fee_params.config().compute_overhead_part,
-                pubdata_overhead_part: fee_params.config().pubdata_overhead_part,
-                batch_overhead_l1_gas: fee_params.config().batch_overhead_l1_gas,
-                max_gas_per_batch: fee_params.config().max_gas_per_batch,
-                max_pubdata_per_batch: fee_params.config().max_pubdata_per_batch,
                 estimate_gas_price_scale_factor,
                 estimate_gas_scale_factor,
+                fee_params,
             },
         }
     }
@@ -55,24 +66,12 @@ impl TestNodeFeeInputProvider {
     }
 
     pub fn get_fee_model_config(&self) -> FeeModelConfigV2 {
-        FeeModelConfigV2 {
-            minimal_l2_gas_price: self.l2_gas_price,
-            compute_overhead_part: self.compute_overhead_part,
-            pubdata_overhead_part: self.pubdata_overhead_part,
-            batch_overhead_l1_gas: self.batch_overhead_l1_gas,
-            max_gas_per_batch: self.max_gas_per_batch,
-            max_pubdata_per_batch: self.max_pubdata_per_batch,
-        }
+        self.fee_params.config()
     }
 
     fn get_params(&self) -> FeeParams {
         // TODO: consider using old fee model for the olds blocks, when forking
-        FeeParams::V2(FeeParamsV2::new(
-            self.get_fee_model_config(),
-            self.l1_gas_price,
-            self.l1_pubdata_price,
-            Default::default(),
-        ))
+        FeeParams::V2(self.fee_params)
     }
 
     pub(crate) fn get_batch_fee_input(&self) -> BatchFeeInput {
@@ -83,21 +82,38 @@ impl TestNodeFeeInputProvider {
         let scale_factor = self.estimate_gas_price_scale_factor;
         self.get_params().scale(scale_factor, scale_factor)
     }
+
+    pub fn gas_price(&self) -> u64 {
+        let (base_fee, _) = derive_base_fee_and_gas_per_pubdata(
+            self.get_batch_fee_input_scaled(),
+            VmVersion::latest(),
+        );
+        base_fee
+    }
+
+    pub fn fair_pubdata_price(&self) -> u64 {
+        self.get_batch_fee_input_scaled().fair_pubdata_price()
+    }
 }
 
 impl Default for TestNodeFeeInputProvider {
     fn default() -> Self {
         Self {
-            l1_gas_price: DEFAULT_L1_GAS_PRICE,
-            l1_pubdata_price: DEFAULT_FAIR_PUBDATA_PRICE,
-            l2_gas_price: DEFAULT_L2_GAS_PRICE,
-            compute_overhead_part: 0.0,
-            pubdata_overhead_part: 1.0,
-            batch_overhead_l1_gas: 800000,
-            max_gas_per_batch: 200000000,
-            max_pubdata_per_batch: 500000,
             estimate_gas_price_scale_factor: DEFAULT_ESTIMATE_GAS_PRICE_SCALE_FACTOR,
             estimate_gas_scale_factor: DEFAULT_ESTIMATE_GAS_SCALE_FACTOR,
+            fee_params: FeeParamsV2::new(
+                FeeModelConfigV2 {
+                    minimal_l2_gas_price: DEFAULT_L2_GAS_PRICE,
+                    compute_overhead_part: 0.0,
+                    pubdata_overhead_part: 1.0,
+                    batch_overhead_l1_gas: 800000,
+                    max_gas_per_batch: 200000000,
+                    max_pubdata_per_batch: 500000,
+                },
+                DEFAULT_L1_GAS_PRICE,
+                DEFAULT_FAIR_PUBDATA_PRICE,
+                BaseTokenConversionRatio::default(),
+            ),
         }
     }
 }
