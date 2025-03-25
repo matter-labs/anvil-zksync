66907d7f0fd2f89f75d6ceb96fdcb5fb64671525
Daniyar Itegulov <ditegulov@gmail.com>
2025-03-25 19:16:37 +1100

    chore: reproducible tar archives

--- scripts/refresh_contracts.sh
+++ scripts/refresh_contracts.sh
@@ -3,9 +3,6 @@ set -xe
 
 BUILTIN_CONTRACTS_OUTPUT_PATH=crates/core/src/deps/contracts/builtin-contracts-v27.tar.gz
 
-# Prepare `tar` command that will generate the output archive
-cmd="tar -czvf $BUILTIN_CONTRACTS_OUTPUT_PATH"
-
 # Forge JSON artifacts to be packed in the archive
 L1_ARTIFACTS_SRC_DIR=contracts/l1-contracts/zkout
 L2_ARTIFACTS_SRC_DIR=contracts/l2-contracts/zkout
@@ -26,27 +23,53 @@ bootloaders=(
 )
 
 for artifact in "${l1_artifacts[@]}"; do
-  cmd="$cmd $L1_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
+  FILES="$FILES $L1_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
 done
 
 for artifact in "${l2_artifacts[@]}"; do
-  cmd="$cmd $L2_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
+  FILES="$FILES $L2_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
 done
 
 for artifact in "${system_contracts_sol[@]}"; do
-  cmd="$cmd $SYSTEM_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
+  FILES="$FILES $SYSTEM_ARTIFACTS_SRC_DIR/$artifact.sol/$artifact.json"
 done
 
 for artifact in "${system_contracts_yul[@]}"; do
-  cmd="$cmd $SYSTEM_ARTIFACTS_SRC_DIR/$artifact.yul/$artifact.json"
+  FILES="$FILES $SYSTEM_ARTIFACTS_SRC_DIR/$artifact.yul/$artifact.json"
 done
 
 for precompile in "${precompiles[@]}"; do
-  cmd="$cmd $SYSTEM_ARTIFACTS_SRC_DIR/$precompile.yul/$precompile.json"
+  FILES="$FILES $SYSTEM_ARTIFACTS_SRC_DIR/$precompile.yul/$precompile.json"
 done
 
 for bootloader in "${bootloaders[@]}"; do
-  cmd="$cmd $SYSTEM_ARTIFACTS_SRC_DIR/$bootloader.yul/$bootloader.json"
+  FILES="$FILES $SYSTEM_ARTIFACTS_SRC_DIR/$bootloader.yul/$bootloader.json"
 done
 
-eval "$cmd"
+# Make sure we are using GNU tar
+case "$(uname -s)" in
+    # Vast majority of Linux distributives have GNU tar installed
+    Linux*)     GNU_TAR_BIN=tar;;
+    # macOS comes with BSD tar but GNU tar is installable as gtar (available in Github runners by default)
+    Darwin*)    GNU_TAR_BIN=gtar;;
+    # Unknown, assuming `tar`
+    *)          GNU_TAR_BIN=tar;;
+esac
+
+# Create reproducible GNU tar archives as per https://www.gnu.org/software/tar/manual/html_section/Reproducibility.html
+cd contracts
+SOURCE_EPOCH=$(TZ=UTC0 git log -1 \
+  --format=tformat:%cd \
+  --date=format:%Y-%m-%dT%H:%M:%SZ)
+cd ..
+TARFLAGS="
+  --sort=name --format=posix
+  --pax-option=exthdr.name=%d/PaxHeaders/%f
+  --pax-option=delete=atime,delete=ctime
+  --clamp-mtime --mtime=$SOURCE_EPOCH
+  --numeric-owner --owner=0 --group=0
+  --mode=go+u,go-w
+"
+GZIPFLAGS="--no-name --best"
+
+LC_ALL=C $GNU_TAR_BIN $TARFLAGS -cvf - $FILES | gzip $GZIPFLAGS > $BUILTIN_CONTRACTS_OUTPUT_PATH
