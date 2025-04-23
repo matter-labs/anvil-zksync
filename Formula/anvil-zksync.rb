ef8286fd147c2f394de28f036e35b812a0c83f26
dutterbutter <29983536+dutterbutter@users.noreply.github.com>
2025-04-23 17:09:09 +0000

    chore: bump homebrew formula to 0.5.1

--- Formula/anvil-zksync.rb
+++ Formula/anvil-zksync.rb
@@ -1,25 +1,25 @@
 class AnvilZksync < Formula
     desc "An in-memory ZKSync node for fast Elastic Network ZK chain development"
     homepage "https://github.com/matter-labs/anvil-zksync"
-    version "0.5.0"
+    version "0.5.1"
   
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-aarch64-apple-darwin.tar.gz"
-        sha256 "a4ad1652175be943aeac78a9c3d82eb3e4d27ddf7ed4544dbc8069d38351fbf5"
+        sha256 "b374c9e2874d65aeeb90aae6ba04a81fbf89ee6fd61add8f6711472986a66aac"
       else
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-x86_64-apple-darwin.tar.gz"
-        sha256 "9f705173914ce6f8426fb344463a7aaa1a5dae8836618388c3a79e098cd7bb60"
+        sha256 "35bb3a9fb801570ce758ab873f8c87038aecf2dd1bd016587ae04c1cfbcc790e"
       end
     end
   
     on_linux do
       if Hardware::CPU.arm?
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "c6a58484d403696d2cc1d250e0ca126eccc13ea4910105822a5bbaf4c2ab229a"
+        sha256 "1edb7b4ad49f0896ddf893defb35ca7e1c6a31f1f87df7503468b3ba8d6f79ae"
       else
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "064327e8dfa58f47d6bb5723a9b193e8eeda37319c7fc8a4041ea2564766a3d1"
+        sha256 "a16d1652a5bf1b319c4b138cc4a63393bae23647bb535308a3bec58885226aa1"
       end
     end
   
