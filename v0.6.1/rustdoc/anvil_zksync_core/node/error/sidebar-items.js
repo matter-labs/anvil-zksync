f74148482ea4bcd7812ac562147ef86de7da5a5d
Deploy from CI <>
2025-04-24 17:14:03 +0000

    Deploy 4d1d8fb

---
+++ v0.6.1/rustdoc/anvil_zksync_core/node/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["LoadStateError"],"trait":["ToHaltError","ToRevertReason"]};
---
+++ v0.6.3/rustdoc/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["anvil_zksync_common",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"anvil_zksync_common/address_map/struct.KNOWN_ADDRESSES.html\" title=\"struct anvil_zksync_common::address_map::KNOWN_ADDRESSES\">KNOWN_ADDRESSES</a>"]]],["zksync_error",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"zksync_error/documentation/struct.model.html\" title=\"struct zksync_error::documentation::model\">model</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[357,311]}
